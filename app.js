01c59abd95fc7b1456cfb54e4102e7c22321e9b7
Harsh Tiwari <129328042+HarshTiwari-err@users.noreply.github.com>
2023-09-28 13:58:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 import user from './routes/user.js'
 import todo from './routes/todo.js'
-
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to My MERN Todo App</h1>');
+});
 app.use('/user', user)
 app.use('/todo', todo)
 
